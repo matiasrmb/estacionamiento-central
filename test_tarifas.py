ddd8902ed15067a7c12fa1c7d273e0f3bcb3811a
Matias <matiasrmb@outlook.com>
2025-06-17 18:58:15 -0400

    Ignora script de prueba test_tarifas.py

--- test_tarifas.py
+++
@@ -1,9 +0,0 @@
-from controllers.tarifas_controller import calcular_tarifa
-
-# Lista de minutos a probar
-minutos_de_prueba = [14, 21, 26, 30, 32, 37, 41, 46, 50, 55, 60, 61, 74, 83, 115, 143, 180]
-
-print("🔎 Prueba de cálculo de tarifas:")
-for minutos in minutos_de_prueba:
-    tarifa = calcular_tarifa(minutos)
-    print(f"{minutos} minutos -> ${tarifa}")
