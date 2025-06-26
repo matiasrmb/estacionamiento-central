4ca68e82f56e1c4a16ab19f40f1313250ebc3ad1
Matias <matiasrmb@outlook.com>
2025-06-25 20:16:40 -0400

    Estilo visual base aplicado a todo el sistema

--- main.py
+++ main.py
@@ -2,8 +2,11 @@ from views.login import LoginWindow
 from PySide6.QtWidgets import QApplication
 import sys
 
+
 if __name__ == "__main__":
     app = QApplication(sys.argv)
+    with open("styles/estilos.qss", "r") as f:
+        app.setStyleSheet(f.read())
     ventana = LoginWindow()
     ventana.show()
     sys.exit(app.exec())
